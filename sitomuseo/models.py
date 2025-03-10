b252cc8cad2c6721398b299b820e7edec0555406
alessandro-grassi <grassi.alessandro06@gmail.com>
2025-03-10 13:48:36 +0100

    corretto 'length'

--- sitomuseo/models.py
+++ sitomuseo/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Spostamento (models.Model):
-    coordinates = models.CharField(max_lenght=100)
+    coordinates = models.CharField(max_length=100)
 
     def where(self):
         return self.coordinates
 
 class Sala (models.Model):
-    # spheric_img = models.CharField(max_lenght=100)
-    description = models.CharField(max_lenght=100)
+    # spheric_img = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
 
     def get_desc(self):
         return self.description
@@ -18,8 +18,8 @@ class Sala (models.Model):
         return self.spheric_img
 
 class PuntoDiInteresse (models.Model):
-    name = models.CharField(max_lenght=100)
-    coordinates = models.CharField(max_lenght=100)
+    name = models.CharField(max_length=100)
+    coordinates = models.CharField(max_length=100)
 
     def where(self):
         return self.coordinates
@@ -28,9 +28,9 @@ class PuntoDiInteresse (models.Model):
         return self.name
 
 class Opera (models.Model):
-    name = models.CharField(max_lenght=100)
-    description = models.CharField(max_lenght=100)
-    # model3D = models.CharField(max_lenght=100)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    # model3D = models.CharField(max_length=100)
 
     def get_desc(self):
         return self.description
@@ -42,8 +42,8 @@ class Opera (models.Model):
         return self.model3D
 
 class Collezione (models.Model):
-    name = models.CharField(max_lenght=100)
-    description = models.CharField(max_lenght=100)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
 
     def get_desc(self):
         return self.description
@@ -52,12 +52,12 @@ class Collezione (models.Model):
         return self.name
 
 class Autore (models.Model):
-    name = models.CharField(max_lenght=100)
-    descrizione = models.CharField(max_lenght=100)
-    nazionalita = models.CharField(max_lenght=100)
-    data_nascita = models.CharField(max_lenght=100)
-    data_morte = models.CharField(max_lenght=100)
-    eta = models.CharField(max_lenght=100)
+    name = models.CharField(max_length=100)
+    descrizione = models.CharField(max_length=100)
+    nazionalita = models.CharField(max_length=100)
+    data_nascita = models.CharField(max_length=100)
+    data_morte = models.CharField(max_length=100)
+    eta = models.CharField(max_length=100)
 
     def get_desc(self):
         return self.description

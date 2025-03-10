c450984700797c84086f4dd866d54727ec5055c0
alessandro-grassi <grassi.alessandro06@gmail.com>
2025-03-10 13:39:15 +0100

    creati modelli base

--- sitomuseo/models.py
+++ sitomuseo/models.py
@@ -1,7 +1,81 @@
 from django.db import models
 
 # Create your models here.
-class prova:
+class Spostamento (models.Model):
+    coordinates = models.CharField(max_lenght=100)
+
+    def where(self):
+        return self.coordinates
+
+class Sala (models.Model):
+    # spheric_img = models.CharField(max_lenght=100)
+    description = models.CharField(max_lenght=100)
+
+    def get_desc(self):
+        return self.description
+
+    def get_model(self):
+        return self.spheric_img
+
+class PuntoDiInteresse (models.Model):
+    name = models.CharField(max_lenght=100)
+    coordinates = models.CharField(max_lenght=100)
+
+    def where(self):
+        return self.coordinates
+
+    def get_name(self):
+        return self.name
+
+class Opera (models.Model):
+    name = models.CharField(max_lenght=100)
+    description = models.CharField(max_lenght=100)
+    # model3D = models.CharField(max_lenght=100)
+
+    def get_desc(self):
+        return self.description
+
+    def get_name(self):
+        return self.name
+
+    def get_model(self):
+        return self.model3D
+
+class Collezione (models.Model):
+    name = models.CharField(max_lenght=100)
+    description = models.CharField(max_lenght=100)
+
+    def get_desc(self):
+        return self.description
+
+    def get_name(self):
+        return self.name
+
+class Autore (models.Model):
+    name = models.CharField(max_lenght=100)
+    descrizione = models.CharField(max_lenght=100)
+    nazionalita = models.CharField(max_lenght=100)
+    data_nascita = models.CharField(max_lenght=100)
+    data_morte = models.CharField(max_lenght=100)
+    eta = models.CharField(max_lenght=100)
+
+    def get_desc(self):
+        return self.description
+
+    def get_name(self):
+        return self.name
+
+    def get_nazionalita(self):
+        return self.nazionalita
+
+    def get_data_nascita(self):
+        return self.data_nascita
+
+    def get_data_morte(self):
+        return self.data_morte
+
     '''
-        classe prova
+    def get_eta(self):
+        return self.eta
     '''
+

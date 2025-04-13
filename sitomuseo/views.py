bc2c41755673d733bf77ff9a316812ade9ac3a22
zEmaaah <emanuelelegnani006@gmail.com>
2025-04-13 12:15:34 +0200

    home e detail opera

--- sitomuseo/views.py
+++ sitomuseo/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+from .models import Opera
 
-# Create your views here.
+def home(request):
+    opere = Opera.objects.all()
+    return render(request, 'sitomuseo/home.html', {'opere': opere})
 
+def detail(request, opera_id):
+    opera = get_object_or_404(Opera, id=opera_id)
+    return render(request, 'sitomuseo/dettaglio_opera.html', {'opera': opera})
+    
 def placeholder(request):
     return render(request,"placeholder.html")

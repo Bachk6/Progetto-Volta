b57260c070644ddeb7f8daf60ea106b01f963565
alessandro-grassi <grassi.alessandro06@gmail.com>
2025-04-14 12:42:57 +0200

    aggiunta model autore e crosswords con chiavi

--- sitomuseo/models.py
+++ sitomuseo/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 
+class Autore(models.Model):
+    name = models.CharField(max_length=200)
+    nationality = models.CharField(max_length=200)
+    birth_date = models.DateField()
+    death_date = models.DateField()
+    description = models.TextField()
+    photo = models.ImageField(upload_to='media/imgsrc/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
 class Opera(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100) # opzionale
+    author = models.ForeignKey(Autore ,on_delete=models.CASCADE)
     year = models.PositiveIntegerField()
     description = models.TextField()
     image = models.ImageField(upload_to='media/imgsrc/', blank=True, null=True)
@@ -10,3 +21,15 @@ class Opera(models.Model):
 
     def __str__(self):
         return self.title
+
+class Crosswords(models.Model):
+    opera = models.OneToOneField(Opera, on_delete=models.CASCADE)
+    autore = models.OneToOneField(Autore, on_delete=models.CASCADE)
+    field = models.CharField(max_length=200)
+    # matrix = . . .
+
+    def __str__(self):
+        return f"Crosswords arguments: {self.opera.title} and {self.autore.name}"
+
+
+    

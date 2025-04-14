392bc6db5412f9da68a3af16a6e6f4bf8800964f
dieconsonni <diego.consonni321@gmail.com>
2025-04-14 12:44:36 +0200

    modifica models campi

--- sitomuseo/admin.py
+++ sitomuseo/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Opera
+from .models import Autore
+
+admin.site.register(Autore)
+
 
 admin.site.register(Opera)
+
+
 
 # Register your models here.

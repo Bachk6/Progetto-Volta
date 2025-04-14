27b22536724baf70729144f7e5a8413bce0db983
dieconsonni <diego.consonni321@gmail.com>
2025-04-14 12:01:45 +0200

    creata classe autore

--- sitomuseo/models.py
+++ sitomuseo/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 
+class Autore(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    birth_date = models.PositiveIntegerField(null=True, blank=True)
+    death_date = models.PositiveIntegerField(null=True, blank=True)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+    
 class Opera(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
+    author = models.ForeignKey(Autore, on_delete=models.SET_NULL, null=True)
     year = models.PositiveIntegerField()
     description = models.TextField()
     image = models.ImageField(upload_to='media/imgsrc/', blank=True, null=True)
 
     def __str__(self):
         return self.title
+    
+
+

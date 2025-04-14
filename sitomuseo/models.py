7a04ebd7b853aa31697a6ccc8bb1db5ce9d06e32
dieconsonni <diego.consonni321@gmail.com>
2025-04-14 13:11:02 +0200

    ampliato modello autore

--- sitomuseo/models.py
+++ sitomuseo/models.py
@@ -3,9 +3,11 @@ from django.db import models
 class Autore(models.Model):
     name = models.CharField(max_length=30)
     surname = models.CharField(max_length=30)
-    birth_date = models.PositiveIntegerField(null=True, blank=True)
-    death_date = models.PositiveIntegerField(null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    death_date = models.DateField(null=True, blank=True)
+    nation = models.CharField(max_length=100)
     description = models.TextField()
+    image = models.ImageField(upload_to='media/imgsrc/', blank=True, null=True)
 
     def __str__(self):
         return self.name+" "+self.surname

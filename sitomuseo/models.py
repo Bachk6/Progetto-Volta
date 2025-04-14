9087c5aa4d2de5892ed4425c677d3dc937d8ae03
alessandro-grassi <grassi.alessandro06@gmail.com>
2025-04-14 13:19:08 +0200

    ciao

--- sitomuseo/models.py
+++ sitomuseo/models.py
@@ -13,11 +13,12 @@ class Autore(models.Model):
 
 class Opera(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Autore ,on_delete=models.CASCADE)
+    author = models.CharField(max_length=300)
+    #models.ForeignKey(Autore ,on_delete=models.CASCADE)
     year = models.PositiveIntegerField()
     description = models.TextField()
     image = models.ImageField(upload_to='media/imgsrc/', blank=True, null=True)
-    model_3D = models.ImageField(upload_to='media/imgsrc/', blank=True, null=True)
+    #model_3D = models.ImageField(upload_to='media/imgsrc/', blank=True, null=True)
 
     def __str__(self):
         return self.title

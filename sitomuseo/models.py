09e43c5aa4c16bee3c032e04173d88c539cb80b9
alessandro-grassi <grassi.alessandro06@gmail.com>
2025-04-14 12:17:55 +0200

    modificata classe opera e template detail

--- sitomuseo/models.py
+++ sitomuseo/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 class Opera(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
+    author = models.CharField(max_length=100) # opzionale
     year = models.PositiveIntegerField()
     description = models.TextField()
     image = models.ImageField(upload_to='media/imgsrc/', blank=True, null=True)
+    model_3D = models.ImageField(upload_to='media/imgsrc/', blank=True, null=True)
 
     def __str__(self):
         return self.title

dba8443ad1aba460fa81de465d1083b3bffecfe9
dieconsonni <diego.consonni321@gmail.com>
2025-04-14 13:08:14 +0200

    tolte minchiate

--- sitomuseo/admin.py
+++ sitomuseo/admin.py
@@ -3,10 +3,8 @@ from .models import Opera
 from .models import Autore
 
 admin.site.register(Autore)
-#cazzo adfasf daf ad s das d
+
 
 admin.site.register(Opera)
-
-
 
 # Register your models here.

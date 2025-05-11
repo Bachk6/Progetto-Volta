07241779811ba7572eb71fdd5331559fc8e71565
alessandro-grassi <grassi.alessandro06@gmail.com>
2025-05-11 19:17:25 +0200

    aggiunti utenti in admin

--- sitomuseo/admin.py
+++ sitomuseo/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Opera)
-
+admin.site.register(Utente)
 admin.site.register(Autore)
 admin.site.register(Achievement)
 admin.site.register(Crossword)
